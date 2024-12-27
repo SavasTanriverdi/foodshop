2291c0c9571b5ee5dadbb40ec9f2bac0eeaa59b4
Savaş Tanrıverdi <137842670+SavasTanriverdi@users.noreply.github.com>
2024-12-27 14:05:46 +0300

    Food Shop App
    Food Shop is an Android application that provides the fastest food delivery service. This project is built using Java and Kotlin, and it uses Gradle for build automation.

---
+++ app/src/main/java/com/example/foodshop/MainActivity.java
@@ -0,0 +1,44 @@
+package com.example.foodshop;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void login_user(View view) {
+        // Burada login işlemlerini gerçekleştirebilirsiniz
+        Toast.makeText(this, "Login button clicked", Toast.LENGTH_SHORT).show();
+
+        // Örneğin, başka bir aktiviteye geçiş yapmak için:
+        // Intent intent = new Intent(this, AnotherActivity.class);
+        // startActivity(intent);
+    }
+
+    public void singup_user(View view) {
+        Toast.makeText(this, "Signup button clicked", Toast.LENGTH_SHORT).show();
+        Intent intent = new Intent(this, SignupActivity.class);
+        startActivity(intent);
+    }
+
+
+}
